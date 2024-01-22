c38712296d4162d94db47863de636c221732cfe7
Salam Nik <salamnik42@gmail.com>
2024-01-22 11:23:50 +0330

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# g++ -c -g -fPIC src/sha256.cpp -o build/sha256.o
-# g++ -c -g -fPIC src/Trie.cpp -o build/Trie.o
-# g++ -c -g -fPIC src/GenealogyGraph.cpp -o build/GenealogyGraph.o
-# g++ -g -shared -o build/GenealogyGraph.so build/GenealogyGraph.o build/Trie.o build/sha256.o
-# g++ -o app app.cpp build/*.o
+for file in src/*.cpp; do
+  g++ -c "$file" -o "build/$(basename "$file" .cpp).o"
+done
 
-g++ -c src/*.cpp
-g++ -o main.exe app.cpp *.o
-# main.exe
+g++ -o main.exe app.cpp build/*.o
 
 echo "Compilation completed successfully."
